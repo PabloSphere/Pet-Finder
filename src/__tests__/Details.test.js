07fc3f0a9ae85d32a82721e54c1c0add3ec1211f
Alan Thompson <alanthompson.se@gmail.com>
2019-03-16 13:38:03 -0400

    Jest testing

--- src/__tests__/Details.test.js
+++ src/__tests__/Details.test.js
@@ -6,3 +6,16 @@ test("snapshot", () => {
   const c = create(<Details />);
   expect(c.toJSON()).toMatchSnapshot();
 });
+
+//Snapshot tests: good to use if you expect the page to stay the same. It
+//essentially takes a snapshot of the page and alerts if it ever changes
+//Good to use for testing API JSON responses
+
+test("show modal when toggleModal is called", () => {
+  const c = create(<Details search={() => {}} />);
+  const instance = c.getInstance();
+
+  expect(instance.state.showModal).toBe(false);
+  instance.toggleModal();
+  expect(instance.state.showModal).toBe(true);
+});
